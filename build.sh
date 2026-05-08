0193279edb4a01d10f0ce105e3e6e27b7cd88989
Subhash Dhaniyala <dhaniyalasubhash@gmail.com>
2026-05-08 23:46:13 +0530

    Fix superuser with env variables

--- build.sh
+++ build.sh
@@ -4,11 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth.models import User
-if not User.objects.filter(username='subhash').exists():
-    User.objects.create_superuser('subhash', 'dhaniyalasubhash@gmail.com', 'Subhash@123')
-    print('Superuser created!')
-else:
-    print('Superuser already exists!')
-"
+python manage.py createsuperuser --noinput || true
