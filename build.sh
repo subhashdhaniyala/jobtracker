7b00a06a2dd27022c5934554926e0e98f18e0591
Subhash Dhaniyala <dhaniyalasubhash@gmail.com>
2026-05-08 09:05:52 +0530

    Create superuser on Render

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(username='subhash').exists() or User.objects.create_superuser('subhash', 'dhaniyalasubhash@gmail.com', 'Subhash@123')" | python manage.py shell
