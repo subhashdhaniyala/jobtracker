4512488ef6897172b023aea51ffb46cfbe1f926b
Subhash Dhaniyala <dhaniyalasubhash@gmail.com>
2026-05-08 23:59:10 +0530

    Switch to PostgreSQL on Render

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,10 +53,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jobtracker.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
