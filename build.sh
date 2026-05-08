b7b67ef5f05223ff1a7fee373173b24362fb2281
Subhash Dhaniyala <dhaniyalasubhash@gmail.com>
2026-05-08 23:37:51 +0530

    Fix superuser creation script

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(username='subhash').exists() or User.objects.create_superuser('subhash', 'dhaniyalasubhash@gmail.com', 'Subhash@123')" | python manage.py shell
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='subhash').exists():
+    User.objects.create_superuser('subhash', 'dhaniyalasubhash@gmail.com', 'Subhash@123')
+    print('Superuser created!')
+else:
+    print('Superuser already exists!')
+"
